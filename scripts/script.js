910e60ed8f05a5847a0525d2929ecde6e02b320e
Mario <maalnu93@hotmail.com>
2023-10-18 09:55:04 +0200

    JS ya terira y deposita

---
+++ scripts/script.js
@@ -0,0 +1,43 @@
+/**
+ * Autor MArio ALfonso Nuñez (DAW)
+ * Github:
+ */
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const saldoInicial = 1000
+    let saldoActual = saldoInicial
+
+    const mensajeElemento = document.getElementById("mensaje")
+    const saldoElemento = document.getElementById("saldo")
+    const importeElemento = document.getElementById("importe")
+    const retirarBoton = document.getElementById("retirar")
+    const depositarBoton = document.getElementById("depositar")
+
+    retirarBoton.addEventListener("click", () => {
+        const cantidad = parseFloat(importeElemento.value)
+        if (isNaN(cantidad) || cantidad <= 0 || cantidad > saldoActual){
+            mensajeElemento.textContent = "Importe no es valido"
+        }else{
+            saldoActual -= cantidad
+            //saldoElemento.textContent = `Saldo: ${saldoActual}€`
+            actualizarSaldo()
+        }
+    })
+
+    depositarBoton.addEventListener("click", () => {
+        const cantidad = parseFloat(importeElemento.value)
+        if (isNaN(cantidad) || cantidad <= 0){
+            mensajeElemento.textContent = "Importe no es valido"
+        }else{
+            saldoActual += cantidad
+           //saldoElemento.textContent = `Saldo: ${saldoActual}€`
+            actualizarSaldo()
+        }
+    })
+
+    const actualizarSaldo = () => {
+        saldoElemento.textContent = `Saldo: ${saldoActual}€`
+    }
+
+})
